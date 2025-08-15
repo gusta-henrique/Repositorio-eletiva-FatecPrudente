6c7aa05967103a02898415dda27edaa9ca0e5fbf
Gustavo Henrique <henrique.gusta777@gmail.com>
2025-08-14 21:01:30 -0300

    Primeiro codigo

---
+++ exemplo/primeiro.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Primeiro codigo PHP</title>
+</head>
+<body>
+    
+<h1>Òla hoje é <?php echo date("d/n/v"); ?> </h1>
+
+</body>
+</html>
