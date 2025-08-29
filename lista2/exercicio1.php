9af98dcd3f1c25e2b72591e7272ca167f92ee4f7
gustavo <henrique.gusta777@gmail.com>
2025-08-29 17:03:42 -0300

    lista2

---
+++ lista2/exercicio1.php
@@ -0,0 +1,13 @@
+<?php
+
+    include("cabecalho.php");
+
+        $numeros = array(1, 2, 3, 0, 5, 6, 7);
+        $menorNumero = min($numeros);
+
+        echo"<p>o menor numero da lista é $menorNumero </p>";
+ 
+    include("rodape.php");
+?>
+
+
