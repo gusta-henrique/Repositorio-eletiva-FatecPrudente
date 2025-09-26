bb43f830a9d84c0d9818f4f7347317f4b02464ab
Gustavo Henrique Costa Pereira <116192541+gusta-henrique@users.noreply.github.com>
2025-09-26 00:09:42 -0300

    exercicio 5

---
+++ lista3/exercicio5.php
@@ -0,0 +1,30 @@
+<?php
+include("cabecalho.php");
+?>
+
+<form method="post">
+    <div class="mb-3">
+        <label for="dia" class="form-label">Informe um valor:</label>
+        <input type="number" id="valor" name="valor" class="form-control" required>
+    </div>
+  
+</form>
+
+<?php
+if ($_SERVER['REQUEST_METHOD'] == "POST") {
+    $valor = $_POST['valor'];
+
+    function RetornaRaiz($valor) {
+        if ($valor < 0) {    
+            echo "<p>valor inválido</p>";
+        }
+
+        return sqrt($valor);
+    }
+    
+    $resultado = RetornaRaiz($valor);
+    echo "<p>A raiz quadrada de $valor é $resultado</p>";
+}
+
+include("rodape.php");
+?>
