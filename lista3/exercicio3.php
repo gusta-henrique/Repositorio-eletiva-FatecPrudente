6ed3e63640608f26d98e9423eef9e991d3849198
Gustavo Henrique Costa Pereira <116192541+gusta-henrique@users.noreply.github.com>
2025-09-25 23:32:49 -0300

    exercicio 3

--- lista3/exercicio3.php
+++ lista3/exercicio3.php
@@ -1,1 +1,25 @@
-//exercicio 3
+<?php
+include("cabecalho.php");
+?>
+<form method="post">
+<div class="mb-3">
+              <label for="palavra1" class="form-label">primeira palavra: </label>
+              <input type="text" id="palavra1" name="palavra1" class="form-control" required="">
+            </div><div class="mb-3">
+              <label for="palavra2" class="form-label">segunda palavra: </label>
+              <input type="text" id="palavra2" name="palavra2" class="form-control" required="">
+            </div>
+<button type="submit" class="btn btn-primary">Enviar</button>
+</form>
+
+<?php
+{   
+    if (strpos($palavra1, $palavra2) !== false) {
+        echo "<p>A segunda palavra está contida na primeira</p>"
+    } else {
+        echo "<p>A segunda palavra não está contida na primeira</p>"
+    }
+    
+    include("rodape.php");
+}
+?>
