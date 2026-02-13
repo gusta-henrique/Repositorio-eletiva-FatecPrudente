bc2ad22a126821d26e5005a7dad902d1b9643650
Gustavo Henrique Costa Pereira <116192541+gusta-henrique@users.noreply.github.com>
2026-02-12 21:59:47 -0300

    Add files via upload

---
+++ PrimeiroProjeto/routes/web.php
@@ -0,0 +1,20 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+
+Route::get('/exercicio', function() {
+    return view('exercicio');
+});
+
+Route::post('/resposta', function(Request $request){
+    $valor1 = $request->input('valor1');
+    $valor2 = $request->input('valor2');
+    $soma =  $valor1 + $valor2;
+    return("a soma é: $soma");
+});
---
+++ PrimeiroProjeto/storage/framework/views/abfd6eb2351763a99ac21d23530d1420.php
@@ -0,0 +1,16 @@
+
+
+<?php $__env->startSection('conteudo'); ?>
+
+<form method="post" action="/resposta">
+    <?php echo csrf_field(); ?>
+<div class="mb-3">
+              <label for="valor1" class="form-label">informe o primeiro valor</label>
+              <input type="number" id="valor1" name="valor1" class="form-control" required="">
+            </div><div class="mb-3">
+              <label for="valor2" class="form-label">informe o segundo valor</label>
+              <input type="text" id="valor2" name="valor2" class="form-control" required="">
+            </div>
+<button type="submit" class="btn btn-primary">Enviar</button>
+</form>
+<?php echo $__env->make('layout', array_diff_key(get_defined_vars(), ['__data' => 1, '__path' => 1]))->render(); ?><?php /**PATH C:\laragon\www\PrimeiroProjeto\resources\views/exercicio.blade.php ENDPATH**/ ?>
