7f53b7cff3c16b26d46f5c1c0c28c355bf953e67
gusta-henrique <henrique.gusta777@gmail.com>
2026-03-26 21:17:15 -0300

    controlles e views

--- projetoP1/routes/web.php
+++ projetoP1/routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
+use App\Http\Controllers\LojaController;
+
+
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::resource('lojas', LojaController::class);
